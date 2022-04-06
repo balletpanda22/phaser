fd24965dfd86167ee7bb13142ecebfa7a3196843
balletpanda22 <94208941+balletpanda22@users.noreply.github.com>
2022-04-06 09:31:03 -0400

    dammmmmiiitttt

--- kriff/game.js
+++ kriff/game.js
@@ -71,7 +71,7 @@ class playGame extends Phaser.Scene{
 
         // adding the text field
         this.prizeText = this.add.text(game.config.width / 2, game.config.height - 20, "Spin the wheel", {
-            font: "32px Playlist",
+            font: "100px Playlist-script",
             align: "center",
             color: "white"
         });
